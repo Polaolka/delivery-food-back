4845fd05a087c17ace59e50232bbebd859d84c56
Olga Polishchuk <olgapolichuk@ukr.net>
2023-06-03 16:54:45 +0300

    fix getAll

--- controllers/foods/getAll.js
+++ controllers/foods/getAll.js
@@ -2,9 +2,8 @@ const { Food } = require("../../models/food");
 
 const getAll = async (req, res) => {
   const { shop_owner_food } = req.body;
-  const conditions = {
-    shop_owner_food,
-  };
+  const conditions = {};
+  if (shop_owner_food !== undefined) conditions.shop_owner_food = shop_owner_food;
   const { page = 1, limit = 6 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Food.find(conditions, "-createdAt -updatedAt", {
